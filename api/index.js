3bdfc947895a4cdd8a3c69401d8b438f54dad692
nanhangl <58681238+nanhangl@users.noreply.github.com>
2021-03-16 16:22:29 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,11 +4,11 @@ module.exports = (req, res) => {
   const { carParkNo } = req.query;
   const base_url = "https://api.data.gov.sg/v1/transport/carpark-availability";
   
-  const tenMinInterval = [];
+  var tenMinInterval = [];
   
   const currentTime = new Date() / 1000;
   
-  for (var interval; interval < 6; interval++) {
+  for (var interval = 0; interval < 6; interval++) {
     tenMinInterval.push(currentTime - (600*interval));
   }
   
