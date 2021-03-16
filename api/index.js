a36f75c3cd9e7610604f0f2b48865235ab5ce2a7
nanhangl <58681238+nanhangl@users.noreply.github.com>
2021-03-17 00:13:01 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,44 +1,5 @@
 const axios = require('axios');
 
 module.exports = (req, res) => {
-  const { carParkNo } = req.query;
-  
-  const base_url = "https://api.data.gov.sg/v1/transport/carpark-availability";
-  
-  var tenMinIntervalData = [];
-  var hourIntervalData = [];
-  var exampleRes = [];
-  
-  const currentTime = new Date() / 1000;
-  
-  for (var interval = 0; interval < 6; interval++) {
-    const intervalTime = new Date(currentTime - (600*interval));
-    axios.get(`${base_url}?date_time=${intervalTime.getFullYear()}-${(intervalTime.getMonth()+1).toString().padStart(2, '0')}-${intervalTime.getDate().toString().padStart(2, '0')}T${intervalTime.getHours().toString().padStart(2, '0')}:${intervalTime.getMinutes().toString().padStart(2, '0')}:${intervalTime.getSeconds().toString().padStart(2, '0')}`)
-    .then(function (response) {
-      console.log(response)
-      exampleRes.push(response);
-      const carpark_data = response.data.items[0].carpark_data;
-      for (var item in carpark_data) {
-        if (carpark_data[item].carpark_number == carParkNo) {
-          tenMinIntervalData.push(carpark_data[item]);
-        }
-      }
-    })
-  }
-  
-  for (var interval = 0; interval < 24; interval++) {
-    const intervalTime = new Date(currentTime - (3600*interval));
-    axios.get(`${base_url}?date_time=${intervalTime.getFullYear()}-${(intervalTime.getMonth()+1).toString().padStart(2, '0')}-${intervalTime.getDate().toString().padStart(2, '0')}T${intervalTime.getHours().toString().padStart(2, '0')}:${intervalTime.getMinutes().toString().padStart(2, '0')}:${intervalTime.getSeconds().toString().padStart(2, '0')}`)
-    .then(function (response) {
-      const carpark_data = response.data.items[0].carpark_data;
-      for (var item in carpark_data) {
-        if (carpark_data[item].carpark_number == carParkNo) {
-          hourIntervalData.push(carpark_data[item]);
-        }
-      }
-    })
-  }
-  axios.get(`${base_url}`).then(response => {
-  res.status(200).send(response);
-  })
+  res.status(200).send('hello');
 }
