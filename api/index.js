7c03303dd2e4330c070aec1305b247b900b2471b
nanhangl <58681238+nanhangl@users.noreply.github.com>
2021-03-16 16:16:03 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+const axios = require('axios');
+
 module.exports = (req, res) => {
-  const { name = 'World' } = req.query
-  res.status(200).send(`Hello ${name}!`)
+  const { carParkNo } = req.query;
+  const base_url = "https://api.data.gov.sg/v1/transport/carpark-availability";
+  
+  const tenMinInterval = [];
+  
+  const currentTime = new Date() / 1000;
+  
+  for (var interval; interval < 6; interval++) {
+    tenMinInterval.push(currentTime - (600*interval));
+  }
+  
+  res.status(200).send(tenMinInterval)
 }
